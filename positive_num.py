fafe3a012694d253de18abc9ad3194a1c952adf5
PraveenADPP <43532070+PraveenADPP@users.noreply.github.com>
2020-10-10 21:43:38 +0530

    Created positive_num.py
    to print positive numbers from the given 2 different list

---
+++ positive_num.py
@@ -0,0 +1,13 @@
+
+list1 = [12, -7, 5, 64, -14] 
+list2 = [12, 14, -95, 3]
+
+for num in list1: 
+	 
+	if num >= 0: 
+	   print ("The positive numbers for List1 ",num) 
+
+for num1 in list2: 
+	 
+	if num1 >= 0: 
+	   print ("The positive numbers for List2 ",num1) 
